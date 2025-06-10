c721b8f1feddbb506daf8812732f51e29de7345e
santiagoahl <santiago.ahumada.1@gmail.com>
2025-06-09 23:43:07 -0500

    feat: pandas toolbox mvp. Include read csv and df query

---
+++ src/tools/pandas_toolbox.py
@@ -0,0 +1,59 @@
+from langchain.tools import tool
+import pandas as pd
+
+@tool
+def read_df(path: str) -> pd.DataFrame:
+    """
+    Read csv file.
+
+    Parameters
+    ----------
+    path : str
+        Path to csv file.
+    
+    Returns:
+        pd.DataFrame: Imported dataframe
+    
+    Example:
+        >>> df = read_df.invoke({"path": "data/agent_experiments/summary.csv"}) 
+        >>> df.shape
+        '(20, 5)'
+    """
+    
+    if not path.endswith(".csv"):
+        return ValueError(f"{path} is not a valid .csv file")
+    return pd.read_csv(path)
+
+@tool
+def query_df(df: pd.DataFrame, query: str) -> pd.DataFrame:
+    """
+    Query DataFrame. Uses pandas.DataFrame.query built-in method.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Data Object
+    query: str
+        Expression
+    
+    Returns:
+        pd.DataFrame: Queried data object
+    
+    Example:
+        >>> queried_df = query_df.invoke({"df": df, "query": "accuracy > 0.1"})
+        >>> queried_df.shape
+        '(10, 5)'
+    """
+    return df.query(query)
+
+
+if __name__ == "__main__":
+    print("Testing module...")
+    print("Importing Data...")
+    df = read_df.invoke({"path": "data/agent_experiments/summary.csv"})  
+    print("-" * 40, "\nImported df:\n")
+    print(df)
+
+    queried_df = query_df.invoke({"df": df, "query": "accuracy > 0.1"})  
+    print("-" * 40, "\nQueried df:\n")
+    print(queried_df)
